44b17e043f52a50909e3bb2f6587248a00bae5c8
yash631 <yash80763@gmail.com>
2024-07-06 23:32:59 +0530

    Data_Ingestion_Complete

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-from CNNClassifier import logger
+from src.CNNClassifier import logger
+from src.CNNClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+# from src.CNNClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+# from src.CNNClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
+# from src.CNNClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
-logger.info("Welcome to LOG")
+
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
