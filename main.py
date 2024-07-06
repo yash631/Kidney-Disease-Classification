266ffaae644bd8e6803a7e4f2a178b034d534af8
yash631 <yash80763@gmail.com>
2024-07-07 01:50:21 +0530

    prep Base model complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.CNNClassifier import logger
 from src.CNNClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-# from src.CNNClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from src.CNNClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 # from src.CNNClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
 # from src.CNNClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
@@ -16,4 +16,20 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
 
