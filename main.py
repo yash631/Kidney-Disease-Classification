31cc5f84566f75771fb9046a99eff9b9b437230b
yash631 <yash80763@gmail.com>
2024-07-07 03:12:01 +0530

    model training complete

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.CNNClassifier import logger
 from src.CNNClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.CNNClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
-# from src.CNNClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
+from src.CNNClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
 # from src.CNNClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
 
@@ -33,3 +33,13 @@ except Exception as e:
 
 
 
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
